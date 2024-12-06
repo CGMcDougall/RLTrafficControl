a0f46161c2cbf21cf9a45eb47d85353b7096b475
CGMcDougall <ConnorMcDougall29@outlook.com>
2024-12-05 21:30:18 -0500

    Fixed it

--- Traffic_env/envs/Matricies.py
+++ Traffic_env/envs/Matricies.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pygame as pg
 from enum import Enum
-
+from data import Data as pd
 
 ##Possible "tiles" (locations) that the env can have
 class Tile(Enum):
@@ -13,11 +13,20 @@ class Tile(Enum):
 
 
 class storage:
+
     # init storage matrices to track locations of cars with 0,1 values
-    def __init__(self, screen_size=700, mat_size=14, Lanes=2):
+    ns_array = []
+    ew_array = []
+    reward_buffer = []      #Matrix to store the cars that have passed through this frame
+
+    #init storage matrices to track locations of cars with 0,1 values
+    def __init__(self, screen_size = 700, mat_size = 14, Lanes = 2):
+
         self.lanes = Lanes
         self.screen_size = screen_size
         self.mat_size = mat_size
+
+        self.Data = pd()
 
         # Pixel lengths
         self.pixel_len = (int(screen_size / self.mat_size))
@@ -30,7 +39,9 @@ class storage:
             for j in range(self.inter_bound[0], self.inter_bound[1] + 1):
                 self.matrix[i, j] = 2
 
-    def getBordering(self, loc, dir):
+
+
+    def getBordering(self,loc,dir):
         x = int(loc[0] + dir[0])
         y = int(loc[1] + dir[1])
 
@@ -39,13 +50,25 @@ class storage:
 
         return self.matrix[y, x]
 
-    def withinIntersectionBounds(self, loc) -> bool:
+
+    def withinIntersectionBounds(self,loc) -> bool:
+
         x = int(loc[0])
         y = int(loc[1])
         # print(self.inter_bound[0],self.inter_bound[1])
 
         return (self.inter_bound[0] <= x and self.inter_bound[1] >= x and self.inter_bound[0] <= y and self.inter_bound[1] >= y)
 
+    def GetCarsWithinRange(self,r : int = 2):
+        all = []
+        for i in range(1,r+1):
+            all.append(self.matrix[self.inter_bound[0]-i,self.inter_bound[0]])
+            all.append(self.matrix[self.inter_bound[1]+i, self.inter_bound[1]])
+
+            all.append(self.matrix[self.inter_bound[1], self.inter_bound[0]-i])
+            all.append(self.matrix[self.inter_bound[0] + i, self.inter_bound[1]+i])
+
+        return all
 
 # Helper Functions
 def indexToCord(self, i, j):
@@ -59,3 +82,4 @@ def cordToIndex(self, i, j):
     y = j / self.pixel_len
 
     return [int(x), int(y)]
+
